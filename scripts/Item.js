970d7bf50d687804fc2ea657649eca7fa1220d1a
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-10 14:34:41 -0500

    item.js creator fn

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,6 +1,23 @@
-const Item = (function() {
-  
-    return {
+/*global cuid, Item */ 
+'use strict';
 
+const Item = (function() {
+  const validateName = function(name){
+    if( name === ''){
+      throw ('Name does not exist');
+    }
+  };
+
+  const create = function(name){
+    return{
+      id: cuid(),
+      name: name,
+      checked : false
     };
+  };
+
+  return {
+    validateName,
+    create
+  };
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-/* global $, shoppingList, cuid */
+/* global $, shoppingList, Item */
 'use strict';
 
 // eslint-disable-next-line no-unused-vars;
 
-
+const itemNames = [ '', 'apples', 'pears' ];
+itemNames.forEach(name => {
+  try {
+    Item.validateName(name);
+    store.items.push(Item.create(name));
+  } catch(error) {
+    console.log('Cannot add item: ' + error.message);
+  }
+});
+shoppingList.render();
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
