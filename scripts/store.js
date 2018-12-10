87d9208f877da1e7846f5cd7f0485504407d59f3
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-10 12:17:04 -0500

    module setup almost

---
+++ scripts/store.js
@@ -0,0 +1,23 @@
+/*global store, cuid*/
+'use strict';
+
+const store = (function(){
+  const foo = 'bar';
+
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false; 
+  const searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+
+
+}());
