7066037b561831da3121cc03fcd235b4db3e1aa7
publikwerker <hello@publikwerks.com>
2018-12-10 11:53:44 -0800

    added shopping list using the item.create.method

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,6 @@
 
 // eslint-disable-next-line no-unused-vars;
 
-const itemNames = [ '', 'apples', 'pears' ];
-itemNames.forEach(name => {
-  try {
-    Item.validateName(name);
-    store.items.push(Item.create(name));
-  } catch(error) {
-    console.log('Cannot add item: ' + error.message);
-  }
-});
-shoppingList.render();
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,4 +1,4 @@
-/* global $ ,store, cuid */
+/* global $, store, cuid, Item */
 'use strict';
 
 // eslint-disable-next-line no-unused-vars
@@ -57,7 +57,15 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      Item.create(itemName);
+      store.items.push(Item.create(itemName));
+    } catch(error) {
+      console.log( 'Cannot add item: {error.message}');
+    }
+    render();
+
   }
   
   function handleNewItemSubmit() {
