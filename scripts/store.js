1e51e50ad1c524ab037dc3a3e285a722ebe3e2f5
publikwerker <hello@publikwerks.com>
2018-12-10 14:29:26 -0800

    trying to find findById solution

--- scripts/store.js
+++ scripts/store.js
@@ -12,39 +12,42 @@ const store = (function(){
   const hideCheckedItems = false; 
   const searchTerm = '';
 
-  const findById =  this.items.find(function(idNum){ 
-    console.log('findById ran');
-    return idNum === items.id;});
+  const findById = (function(id) {
+    const foundItem = items.find(item => item.id === id);
+    return foundItem;
+  }
 
   const addItem = function(name){
     console.log('addItem ran');
     try { 
       Item.validateName(name);
-      this.items.push(Item.create(name));
+      Item.create(name);
+      items.push(Item.create(name));
     } catch(error) {
       console.log('Enter a valid item {error.message}');
     }
+    shoppingList.render();
   };
-  const findAndToggleChecked = function(id){
-    this.findById(id).checked = !this.findById(id).checked;
-  };
+    const findAndToggleChecked = function(id){
+      this.findById(id).checked = !this.findById(id).checked;
+    };
 
-  const findAndUpdateName = (function(idNum, newName){
-    console.log('findAndUpdateName ran');
-    try {
-      Item.validateName(newName);
-      // set variable to the value of 
-      items.findById(idNum).name = newName;
-    } catch(error) {
-      console.log('Enter a valid item {error.message}');
-    }
-  });
+    const findAndUpdateName = (function(idNum, newName){
+      console.log('findAndUpdateName ran');
+      try {
+        Item.validateName(newName);
+        // set variable to the value of 
+        items.findById(idNum).name = newName;
+      } catch(error) {
+        console.log('Enter a valid item {error.message}');
+      }
+    });
 
-  const findAndDelete = function(id){
-    const item= items.findById(id);
-    const deletingIndex = items.findIndex(item);
-    items.splice(deletingIndex, 1);
-  };
+    const findAndDelete = function(id){
+      const item = items.findById(id);
+      const deletingIndex = items.findIndex(item);
+      items.splice(deletingIndex, 1);
+    };
 
   return {
     items,
